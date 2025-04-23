11998c357be6db290be53b7c510094177c435182
grinsh <shoshigrin18@gmail.com>
2025-04-23 23:35:37 +0300

    add atlas connection

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use(cors())
 app.use("/animal", animalRouter)
 
 
+app.get("/",(req,res)=>{
+    res.send("hello from server")
+})
+
 app.listen(PORT, ()=>{
     console.log("Server running...");
     
