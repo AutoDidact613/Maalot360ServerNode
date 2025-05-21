7fccab9546f6f9468968599e320ad6be7c2edfc6
ETI-RASFOVSKY <r0533160762@gmail.com>
2025-05-21 20:54:40 +0300

    Update app.js
    change return

--- app.js
+++ app.js
@@ -1,47 +1,41 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 
-const animalRouter = require("./Routers/animalRouter");
-const courseRouter = require("./Routers/coursesRouter");
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-
-const conn1 = mongoose.createConnection(
-  "mongodb+srv://autodidact:CY0JbInyQLZ70Irr@maalot360db.i2kujcl.mongodb.net/maalot360db?retryWrites=true&w=majority&appName=maalot360db",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-conn1.on("open", () => {
-  console.log("Connected to Maalot360DB (animal)");
-});
-
-const conn2 = mongoose.createConnection(
-  "mongodb+srv://r0533160762:GkkpTBvELwtxkCBc@courses.m5ru6ic.mongodb.net/?retryWrites=true&w=majority&appName=Courses",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-conn2.on("open", () => {
-  console.log("Connected to Courses DB");
-});
+const mymongo = require("mongoose")
+mymongo.connect("mongodb+srv://autodidact:CY0JbInyQLZ70Irr@maalot360db.i2kujcl.mongodb.net/maalot360db?retryWrites=true&w=majority&appName=maalot360db")
+const mydb = mymongo.connection;
+mydb.on("open", ()=>{
+    console.log("mongodb is open!!!");
+    
+})
 
 
-app.use("/animal", animalRouter);
-app.use("/course", courseRouter);
+const express = require("express")
+const cors = require("cors")
 
-app.get("/", (req, res) => {
-  res.send(`
-    <h1>List of available requests in the server</h1>
-    <ul>
-      <li><a href="/animal/getall">/animal/getall</a></li>
-      <li><a href="/course/getall">/course/getall</a></li>
-    </ul>
-  `);
-});
+const animalRouter = require("./Routers/animalRouter")
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const app = express()
+const PORT = 3000
+
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.use("/animal", animalRouter)
+
+
+app.get("/",(req,res)=>{
+    res.send(`<h1>List of request avilable in server</h1>
+              <ul>
+                    <li><a  href="https://maalot360-server.onrender.com/animal/getall" > https://maalot360-server.onrender.com/animal/getall </li>
+              </ul>
+    `)
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server running...");
+    
+})
+
+
+
